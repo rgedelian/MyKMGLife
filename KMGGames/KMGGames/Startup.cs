1b580aafd58e3abe326174c6df8f7c7b958a8ac4
Rob <rgedelian@gmail.com>
2017-11-27 16:35:28 -0700

    KMG Games

---
+++ KMGGames/KMGGames/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KMGGames.Startup))]
+namespace KMGGames
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
