69ee21b5b722de8724913db42ee59b252b852eaf
Rob <rgedelian@gmail.com>
2017-11-28 10:21:38 -0700

    Add validation

--- KMGGames/KMGGames/Controllers/HomeController.cs
+++ KMGGames/KMGGames/Controllers/HomeController.cs
@@ -27,9 +27,172 @@ namespace KMGGames.Controllers
         }
 
         [HttpPost]
-        public ActionResult Answer(int id, int a)
+        public ActionResult Answer(int id, string ans)
+        {
+            ViewBag.QuestionNumber = id;
+
+            switch(id)
+            {
+                case 1:
+                    if (ans == "b")
+                    {
+                        ViewBag.Correct = "y";
+                    } else
+                    {
+                        ViewBag.Answer = "500 Hz";
+                    }
+                    break;
+                case 2:
+                    if (ans == "b")
+                    {
+                        ViewBag.Correct = "y";
+                    }
+                    else
+                    {
+                        ViewBag.Answer = "500 Hz";
+                    }
+                    break;
+                case 3:
+                    if (ans == "b")
+                    {
+                        ViewBag.Correct = "y";
+                    }
+                    else
+                    {
+                        ViewBag.Answer = "500 Hz";
+                    }
+                    break;
+                case 4:
+                    if (ans == "b")
+                    {
+                        ViewBag.Correct = "y";
+                    }
+                    else
+                    {
+                        ViewBag.Answer = "500 Hz";
+                    }
+                    break;
+                case 5:
+                    if (ans == "b")
+                    {
+                        ViewBag.Correct = "y";
+                    }
+                    else
+                    {
+                        ViewBag.Answer = "500 Hz";
+                    }
+                    break;
+                case 6:
+                    if (ans == "b")
+                    {
+                        ViewBag.Correct = "y";
+                    }
+                    else
+                    {
+                        ViewBag.Answer = "500 Hz";
+                    }
+                    break;
+                case 7:
+                    if (ans == "b")
+                    {
+                        ViewBag.Correct = "y";
+                    }
+                    else
+                    {
+                        ViewBag.Answer = "500 Hz";
+                    }
+                    break;
+                case 8:
+                    if (ans == "b")
+                    {
+                        ViewBag.Correct = "y";
+                    }
+                    else
+                    {
+                        ViewBag.Answer = "500 Hz";
+                    }
+                    break;
+                case 9:
+                    if (ans == "b")
+                    {
+                        ViewBag.Correct = "y";
+                    }
+                    else
+                    {
+                        ViewBag.Answer = "500 Hz";
+                    }
+                    break;
+                case 10:
+                    if (ans == "b")
+                    {
+                        ViewBag.Correct = "y";
+                    }
+                    else
+                    {
+                        ViewBag.Answer = "500 Hz";
+                    }
+                    break;
+                case 11:
+                    if (ans == "b")
+                    {
+                        ViewBag.Correct = "y";
+                    }
+                    else
+                    {
+                        ViewBag.Answer = "500 Hz";
+                    }
+                    break;
+                case 12:
+                    if (ans == "b")
+                    {
+                        ViewBag.Correct = "y";
+                    }
+                    else
+                    {
+                        ViewBag.Answer = "500 Hz";
+                    }
+                    break;
+                case 13:
+                    if (ans == "b")
+                    {
+                        ViewBag.Correct = "y";
+                    }
+                    else
+                    {
+                        ViewBag.Answer = "500 Hz";
+                    }
+                    break;
+                case 14:
+                    if (ans == "b")
+                    {
+                        ViewBag.Correct = "y";
+                    }
+                    else
+                    {
+                        ViewBag.Answer = "500 Hz";
+                    }
+                    break;
+                case 15:
+                    if (ans == "b")
+                    {
+                        ViewBag.Correct = "y";
+                    }
+                    else
+                    {
+                        ViewBag.Answer = "500 Hz";
+                    }
+                    break;
+            }
+
+            return View();
+
+        }
+
+        [HttpGet]
+        public ActionResult End(int id, string ans)
         {
             return View();
+
         }
     }
 }
